47296a7ec2c042ef0dd2a7c879ab93b80d9061c7
Vova Pytsiuk <vovawed@gmail.com>
2025-04-30 20:09:29 -0700

    Reformat `app.py`

--- api/app.py
+++ api/app.py
@@ -1,5 +1,3 @@
-# filename: app.py
-
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import torch
@@ -12,40 +10,37 @@ from pydantic import BaseModel
 class ResponseBody(BaseModel):
     is_potato: bool
 
-# ====== Initialize FastAPI ======
+
 app = FastAPI()
 
-# ====== Load Models ======
-# Load backbone
-feature_extractor = timm.create_model('nextvit_small.bd_ssld_6m_in1k_384', pretrained=True)
-feature_extractor.eval()
 
-# Load logistic regression
-logreg = joblib.load('potato_logreg.pkl')
+# Load models
+emb_model = timm.create_model("nextvit_small.bd_ssld_6m_in1k_384", pretrained=True)
+emb_model.eval()
+potato_model = joblib.load("potato_logreg.pkl")
 
-# Classes
-class_names = ['not_potato', 'potato']
 
 # Preprocessing
-transform = transforms.Compose([
-    transforms.Resize((384, 384)),
-    transforms.ToTensor(),
-])
+transform = transforms.Compose(
+    [
+        transforms.Resize((384, 384)),
+        transforms.ToTensor(),
+    ]
+)
 
-# ====== Define Endpoint ======
+
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)) -> ResponseBody:
-    # Load image
-    image = Image.open(file.file).convert('RGB')
-    img_tensor = transform(image).unsqueeze(0)  # (1, 3, 384, 384)
-    
-    # Extract features
-    with torch.no_grad():
-        features = feature_extractor.forward_features(img_tensor)
-        features = features.mean(dim=[2, 3])  # (1, 960)
+    # Load and transform image
+    image = Image.open(file.file).convert("RGB")
+    img_tensor = transform(image).unsqueeze(0)
 
-    # Predict
-    preds = logreg.predict(features.cpu().numpy())
-    label = class_names[preds[0]]
-    
-    return {'is_potato': label == 'potato'}
+    # Get embeddings
+    with torch.no_grad():
+        features = emb_model.forward_features(img_tensor)
+        features = features.mean(dim=[2, 3])
+
+    preds = potato_model.predict(features.cpu().numpy())
+    is_potato = preds[0]
+
+    return {"is_potato": is_potato}
