0b4843df1e6b585def8f10d4c3809ee3b6e4705b
Vova Pytsiuk <vovawed@gmail.com>
2025-05-01 17:16:59 -0700

    Add `base:` to vite to fix 404 error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/potato/',  // Needed for GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
