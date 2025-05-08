b1567c5466a4f6fc7229598ea198ec913172f28d
Aalizelau <aalizelau@gmail.com>
2025-05-08 12:48:23 -0700

    update inference format to onnx

--- api/app.py
+++ api/app.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, File, UploadFile
-from PIL import Image
-import torch
-import timm
-import joblib
-from torchvision import transforms
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from potatoPredictor import PotatoPredictor
 
 
 class ResponseBody(BaseModel):
     is_potato: bool
+    probability: float
 
 
 app = FastAPI()
@@ -27,33 +24,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load models
-emb_model = timm.create_model("nextvit_small.bd_ssld_6m_in1k_384", pretrained=True)
-emb_model.eval()
-potato_model = joblib.load("potato_logreg.pkl")
-
-
-# Preprocessing
-transform = transforms.Compose(
-    [
-        transforms.Resize((384, 384)),
-        transforms.ToTensor(),
-    ]
-)
-
-
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)) -> ResponseBody:
-    # Load and transform image
-    image = Image.open(file.file).convert("RGB")
-    img_tensor = transform(image).unsqueeze(0)
+    # Initialize the predictor with ONNX model path
+    predictor = PotatoPredictor("nextvit_potatoclassifer.onnx")
 
-    # Get embeddings
-    with torch.no_grad():
-        features = emb_model.forward_features(img_tensor)
-        features = features.mean(dim=[2, 3])
+    # Make a prediction on an image
+    is_potato, prob = predictor.predict(file.file)
 
-    preds = potato_model.predict(features.cpu().numpy())
-    is_potato = preds[0]
-
-    return {"is_potato": is_potato}
+    return {
+        "is_potato": is_potato,         
+        "probability": round(prob, 4)   
+    }
