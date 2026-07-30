0d8e4283b72fd92be13b762f038135862bc48b23
Vova <vovawed@gmail.com>
2026-07-29 17:00:51 -0700

    Move Potato analytics to byvova.com

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,13 @@
 import './assets/main.css'
 
+import posthog from 'posthog-js'
 import { createApp } from 'vue'
 import App from './App.vue'
+
+posthog.init('phc_vmyGKk9d2D9GzpJZH6p5YWW67cP4Qnndt2miMWejurwi', {
+  api_host: 'https://pineapple.byvova.com',
+  defaults: '2026-05-30'
+})
+posthog.register({ project: 'potato' })
 
 createApp(App).mount('#app')
